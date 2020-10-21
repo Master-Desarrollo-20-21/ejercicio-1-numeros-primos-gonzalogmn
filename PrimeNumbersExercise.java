8d3e77a1c228f084afb971409b6d2f3cdb8844a7
Gonzalo Gimeno <gonzalo.gimeno.torres@gmail.com>
2020-10-21 14:41:59 +0200

    Create PrimeNumbersExercise.java

---
+++ PrimeNumbersExercise.java
@@ -0,0 +1,43 @@
+public class PrimeNumbersExercise {
+
+    public static void main(String[] args) {
+        // La suma de los números primos que hay entre los 50 primeros números
+        int maxValue = 50;
+        int result = 0;
+
+        for(int i=1; i<=maxValue; i++) {
+            if(isPrime(i)) {
+                result += i;
+            }
+        }
+
+        System.out.println("La suma de los números primos que hay entre los 50 primeros números es " + result);
+
+        // La suma de los 50 primeros números primos
+        final int MAX_PRIME_COUNT = 50;
+        int primeCount = 0;
+        int actualNumber = 1;
+        result = 0;
+
+        while(primeCount < MAX_PRIME_COUNT) {
+            if(isPrime(actualNumber)) {
+                primeCount++;
+                result += actualNumber;
+            }
+
+            actualNumber++;
+        }
+
+        System.out.println("La suma de los 50 primeros números primos es " + result);
+
+    }
+
+    public static boolean isPrime(int number) {
+        for(int i=2; i<number; i++) {
+            if(number%i==0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
